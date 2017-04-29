e5da110b72252e471fdb297499e3022512f050fd
Rostard-ub <kos.boykow@gmail.com>
2017-04-29 09:55:43 +0300

    Add demonization and log

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 
+#include <fstream>
 char* serv_ip="127.0.0.1";
 int serv_port=11180;
 char* serv_directory;
@@ -27,6 +26,7 @@ void getParam(int argc, char** argv){
 
 int main(int argc, char** argv) {
     //getParam(argc,argv);
+    std::ofstream out("log.txt");
     int fd=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
     sockaddr_in addr;
     addr.sin_family=AF_INET;
@@ -34,17 +34,21 @@ int main(int argc, char** argv) {
     addr.sin_addr.s_addr=htonl(INADDR_ANY);
     //inet_aton(serv_ip,&addr.sin_addr);
     int err= bind(fd,(sockaddr*)(&addr),sizeof(addr));
-    std::cout<<err;
     err=listen(fd,10);
-    std::cout<<err;
+    if(fork()) {
+        std::cout<<"if";
+        out << "Kill main" << std::endl;
+        return 0;
+    }
     int conn_fd=accept(fd,NULL,NULL);
     char buf[1024];
-    while(true){
-        int len=recv(conn_fd,buf,1024,0);
-        std::cout<<buf<<std::endl;
+    int len;
+
+    while((len=recv(conn_fd,buf,1024,0))>0){
+        out<<buf<<std::endl;
     }
 
     std::cout << "Hello, World!" << std::endl;
-
+    out.close();
     return 0;
 }
